fe9c21055d8462712c52880b71ae556fccf702de
Janko Marohnić <janko.marohnic@gmail.com>
2013-10-18 00:13:44 +0200

    Remove Bundler.require
    http://myronmars.to/n/dev-blog/2012/12/5-reasons-to-avoid-bundler-require

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(:assets)
+
+require "simple_form"
+require "haml-rails"
+
+if Rails.env.development?
+  require "pry-rails"
+  require "better_errors"
+end
 
 module Todo
   class Application < Rails::Application
